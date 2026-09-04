c743a2dfbb1a61297d3912bb36581aa2cac0bd7b
L0TUS121 <dmitrobilous56@gmail.com>
2026-09-04 10:16:58 +0300

    Create settings_tile.dart

---
+++ settings_tile.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class SettingsTile extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final String subtitle;
+  final VoidCallback onTap;
+
+  const SettingsTile({
+    super.key,
+    required this.icon,
+    required this.title,
+    required this.subtitle,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      contentPadding: const EdgeInsets.symmetric(horizontal: 14, vertical: 4),
+
+      leading: Container(
+        width: 42,
+        height: 42,
+        decoration: BoxDecoration(
+          color: Colors.grey.shade100,
+          borderRadius: BorderRadius.circular(13),
+        ),
+        child: Icon(icon),
+      ),
+
+      title: Text(title, style: const TextStyle(fontWeight: FontWeight.w600)),
+
+      subtitle: Text(subtitle),
+
+      trailing: const Icon(Icons.chevron_right, color: Colors.grey),
+
+      onTap: onTap,
+    );
+  }
+}
