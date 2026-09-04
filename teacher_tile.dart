5b2b1eecc4b5e7cd8500c03e91ac07af5917bd84
L0TUS121 <dmitrobilous56@gmail.com>
2026-09-04 10:13:11 +0300

    Create teacher_tile.dart

---
+++ teacher_tile.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class TeacherTile extends StatelessWidget {
+  final String type;
+  final String name;
+  final String phone;
+
+  const TeacherTile({
+    super.key,
+    required this.type,
+    required this.name,
+    required this.phone,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+
+      decoration: BoxDecoration(
+        color: Colors.grey.shade100,
+        borderRadius: BorderRadius.circular(18),
+      ),
+
+      child: Row(
+        children: [
+          const CircleAvatar(
+            backgroundColor: Colors.black,
+            foregroundColor: Colors.white,
+            child: Icon(Icons.person_outline),
+          ),
+
+          const SizedBox(width: 14),
+
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+
+              children: [
+                Text(
+                  type,
+                  style: const TextStyle(fontSize: 12, color: Colors.grey),
+                ),
+
+                Text(name, style: const TextStyle(fontWeight: FontWeight.w600)),
+
+                const SizedBox(height: 2),
+
+                Text(phone, style: const TextStyle(color: Colors.grey)),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
