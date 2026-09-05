119239c9036364201a89a47555e9ad0cae6a3b87
L0TUS121 <dmitrobilous56@gmail.com>
2026-09-05 21:50:42 +0300

    Remove ServiceTile widget from main_page.dart
    Removed the ServiceTile widget from main_page.dart.

--- main_page.dart
+++ main_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'home_page.dart';
 import 'schedule_page.dart';
 import 'subjects_page.dart';
+import '../widgets/service_tile.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -118,84 +119,4 @@ void showServicesSheet(BuildContext context) {
       );
     },
   );
-}
-
-class ServiceTile extends StatelessWidget {
-  final IconData icon;
-  final String title;
-  final String subtitle;
-  final VoidCallback onTap;
-
-  const ServiceTile({
-    super.key,
-    required this.icon,
-    required this.title,
-    required this.subtitle,
-    required this.onTap,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 10),
-
-      child: Material(
-        color: Colors.grey.shade100,
-        borderRadius: BorderRadius.circular(18),
-
-        child: InkWell(
-          borderRadius: BorderRadius.circular(18),
-          onTap: onTap,
-
-          child: Padding(
-            padding: const EdgeInsets.all(15),
-
-            child: Row(
-              children: [
-                Container(
-                  width: 44,
-                  height: 44,
-
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                    borderRadius: BorderRadius.circular(14),
-                  ),
-
-                  child: Icon(icon, color: Colors.white),
-                ),
-
-                const SizedBox(width: 14),
-
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-
-                    children: [
-                      Text(
-                        title,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.w600,
-                          fontSize: 15,
-                        ),
-                      ),
-
-                      Text(
-                        subtitle,
-                        style: const TextStyle(
-                          color: Colors.grey,
-                          fontSize: 13,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-
-                const Icon(Icons.open_in_new, size: 18, color: Colors.grey),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
 }
